7be3920c41d92f149cdbefe07ef843c33d8811b9
Gonçalo <gapfranco@gmail.com>
2021-02-15 12:29:59 -0300

    OUTRA NO DEV

--- lib/offers.ex
+++ lib/offers.ex
@@ -5,5 +5,9 @@ defmodule BankAccount do
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
+
+
+  TESTE 1 TESTE 1 TESTE 1 TESTE 1 TESTE 1 TESTE 1 TESTE 1
+  TESTE 1 TESTE 1 TESTE 1 TESTE 1
   """
 end
